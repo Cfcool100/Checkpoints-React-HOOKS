acff45123f1c04555fe69777fd818af605d03d54
Youssouf SISSOKOmrsissoko@Sissokos-MacBook-Pro ~ % git config --global user.name Youssouf <cfcool100@gmail.com>
2023-05-03 14:32:16 +0000

    first

---
+++ src/component/movieList.jsx
@@ -0,0 +1,28 @@
+import React,{useState} from 'react';
+import Card from './movieCard';
+
+const MovieList = ({ films }) => {
+    const [searchTerm, setSearchTerm] = useState("");
+
+    return (
+        <>
+        <div className="search">
+                <input onChange={e => setSearchTerm(e.target.value)} type="search" name="search" id="" />
+            </div>
+        <div className='cardFilm'>
+            {films.filter((film) => {
+                return film.title.toLowerCase().includes(searchTerm)
+            }).map((film) => {
+                return (
+                    <div className='box'>
+                        <Card key={film.rating} film={film} />
+                    </div>
+                )
+            })}
+        </div>
+        </>
+    );
+};
+
+export default MovieList;
+
